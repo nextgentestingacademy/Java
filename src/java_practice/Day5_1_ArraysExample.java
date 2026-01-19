26f78c886aa6144e0edda595f7cb8c3db7ca9a9a
nextgentestingacademy <nextgentestingacademy@gmail.com>
2026-01-19 22:22:09 +0530

    new code

--- src/java_practice/Day5_1_ArraysExample.java
+++ src/java_practice/Day5_1_ArraysExample.java
@@ -4,24 +4,24 @@ public class Day5_1_ArraysExample {
 
 	public static void main(String[] args) {
 		//declaring an array
-		String[] browsers = new String[3];
-		browsers[0] = "chrome";
-		browsers[1] = "firefox";
-		browsers[2] = "edge";
+//		String[] browsers = new String[3];
+//		browsers[0] = "chrome";
+//		browsers[1] = "firefox";
+//		browsers[2] = "edge";
 //		System.out.println(browsers[2]);
 //		browsers[2] = "safari";
 //		System.out.println(browsers[2]);
 		
-		for(int i=0;i<browsers.length;i++) {
-			System.out.println(browsers[i]);
-		}
-		
-		for(String s:browsers) {
-			System.out.println(s);
-		}
+//		for(int i=0;i<browsers.length;i++) {
+//			System.out.println(browsers[i]);
+//		}
+//		
+//		for(String s:browsers) {
+//			System.out.println(s);
+//		}
 		
 //		System.out.println(browsers[3]);
-//		String[] browsers = {"chrome","firefox","edge"};
+		String[] browsers = {"chrome","firefox","edge"};
 		
 //		int[] numbers = {12,34,23};
 //		
@@ -29,6 +29,12 @@ public class Day5_1_ArraysExample {
 ////		System.out.println(brow);
 //		System.out.println(browsers[0]);
 
+		String[][] users = {{"rahul.kamat","pass@123"},
+							{"sachin.sharma","pass@321"}};
+		
+		System.out.println(users[1][0]);
+		
+		
 		
 	}
 
